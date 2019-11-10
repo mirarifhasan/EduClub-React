f76f66e7d023b343f6a156028de5c9d4f20ca92d
mirarifhsan <arif.ishan05@gmail.com>
2019-11-10 18:38:30 +0600

    feature, footer component done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import Feature from './Components/Feature/Feature'
+import Footer from './Components/Footer/Footer'
 
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <p>hi</p>
+        <Feature />
+        <Footer />
       </div>
     )
   }
---
+++ src/Components/Footer/Script.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+export default class Script extends Component {
+    render() {
+        return (
+            <div>
+                <script src="assest/js/vendor/jquery-1.12.4.min.js"></script>
+                <script src="assest/js/vendor/bootstrap.min.js"></script>
+
+                <script src="assest/js/vendor/jquery.easing.1.3.js"></script>
+                <script src="assest/js/vendor/jquery-migrate-1.2.1.min.js"></script>
+                <script src="assest/js/vendor/jquery.appear.js"></script>
+                <script src="assest/js/owl.carousel.min.js"></script>
+                <script src="assest/js/stellar.js"></script>
+                <script src="assest/js/waypoints.min.js"></script>
+                <script src="assest/js/jquery.counterup.min.js"></script>
+                <script src="assest/js/wow.min.js"></script>
+                <script src="assest/js/jquery-modal-video.min.js"></script>
+                <script src="assest/js/stellarnav.min.js"></script>
+                <script src="assest/js/placeholdem.min.js"></script>
+                <script src="assest/js/contact-form.js"></script>
+                <script src="assest/js/jquery.ajaxchimp.js"></script>
+                <script src="assest/js/jquery.sticky.js"></script>
+
+                <script src="assest/js/main.js"></script>
+
+                <script type="text/javascript"
+                    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBTS_KEDfHXYBslFTI_qPJIybDP3eceE-A&sensor=false"></script>
+                <script src="assest/js/maps.active.js"></script>
+            </div>
+        )
+    }
+}
